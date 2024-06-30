3fbd896a57a9a8700dabd2c042d1576b6a3dbb4e
Patrick <163540237+Patty-Github@users.noreply.github.com>
2024-06-30 15:05:09 +0930

    Delete scripts/positionWindow.js

--- scripts/positionWindow.js
+++
@@ -1,5 +0,0 @@
-export function positionWindow(window) {
-    const screen = document.getElementById('screen');
-    window.style.left = (parseFloat(getComputedStyle(screen).width) / 2) - (parseFloat(getComputedStyle(window).width) / 2) + 'px';
-    window.style.top = parseFloat(getComputedStyle(screen).top) + (parseFloat(getComputedStyle(window).height) / 6) - 24 + 'px';
-}
